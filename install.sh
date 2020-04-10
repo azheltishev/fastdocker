2e4ba48f80947cd38ac148fc5f90fbd386eeb803
azheltishev <azheltishevs@gmail.com>
2020-04-11 01:22:43 +0300

    install.sh/uninstall.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo cp ./dg /opt/local/bin/dg
+sudo chmod +x /opt/local/bin/dg
+
+sudo mkdir /opt/local/etc/fastdocker
+sudo cp ./Dockerfile /opt/local/etc/fastdocker/Dockerfile
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+sudo rm /opt/local/bin/dg
+sudo rm -rf /opt/local/etc/fastdocker
